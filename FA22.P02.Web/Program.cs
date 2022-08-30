849f764c911aebf237533eb736dddc17a5af3b02
jordbutler <jordan.butler@selu.edu>
2022-08-30 03:55:57 -0500

    HttpMethods

--- FA22.P02.Web/Program.cs
+++ FA22.P02.Web/Program.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,45 +21,89 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-var names = new[]
-{
-    "Jordan", "Lamarr", "Cory", "Dashie"
-};
+List<Product> products1 = new List<Product>() {
+                new Product(){ Id = 1, Name="Ketchup", Description="Condiment", Price= 4.99},
+                new Product(){ Id = 2, Name="Mustard", Description="Condiment", Price= 2.99},
+                new Product(){ Id = 3, Name="Mayo", Description="Condiment", Price= 3.99},
+                
+            };
 
-app.MapGet("/famoutnames", () =>
+
+
+
+
+app.MapGet("/product/get-product", (int i) =>
 {
-    var name = names[0];
-    return name;
+    var id = products1[i];
+    return id;
+
+
 })
-    .WithName("GetFamousNames");
+    .WithName("GetProduct");
 
-app.MapGet("/weatherforecast", () =>
+
+
+app.MapGet("/products/{get-all}", () =>
 {
-   
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    
+        var Id = products1;
+        return Id;
+    
 })
-.WithName("GetWeatherForecast");
+    .WithName("GetProducts");
+
+app.MapPost("/products/{create-product}", (int i, string name, string description, double price) =>
+{
+    
+    var product =  new Product(){Id = i, Name= name, Description= description, Price= price };
+    products1.Add(product);
+     var Id = products1;
+    return Id;
+
+
+
+
+})
+    .WithName("CreateProduct");
+
+app.MapPut("/products/{update-product}", (int i, string name, string description, double price) =>
+{
+
+    var product = new Product() { Id = i, Name = name, Description = description, Price = price };
+    products1[i] = product;
+    var Id = products1;
+    return Id;
+})
+    .WithName("UpdateProduct");
+
+app.MapDelete("/products/{delete-product}", (int i) =>
+{
+
+    products1.RemoveAt(i);
+    var Id = products1;
+    return Id;
+
+
+})
+    .WithName("DeleteProduct");
+
+
+
+
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
 
 //see: https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-6.0
 // Hi 383 - this is added so we can test our web project automatically. More on that later
-public partial class Program { }
+public partial class Product {
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+
+
+}
